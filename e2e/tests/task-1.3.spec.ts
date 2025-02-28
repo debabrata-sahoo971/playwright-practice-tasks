67387c2f655a04ef2783f913d75dba9a0b26dd4b
Debabrata Sahoo <deba.codingprojects@gmail.com>
2025-02-28 17:41:16 +0000

    Made changes to implement clean

--- e2e/tests/task-1.3.spec.ts
+++ e2e/tests/task-1.3.spec.ts
@@ -1,26 +1,48 @@
-import { test } from '../fixtures'
+import { test } from '../fixtures';
+import { expect } from '@playwright/test'
+import { DASHBOARD_IDS, TEARDOWN_IDS } from '../../constants/test-ids';
 
 test.describe('Dashboard Page', () => {
-
   test.beforeEach(async({page})=>{
     await page.goto('/');
     await page.getByRole('button', { name: 'Login as Oliver' }).click();
+    await page.getByTestId(DASHBOARD_IDS.addFormButton).click();
+    await page.getByTestId(DASHBOARD_IDS.startFromScratch).click();
   });
 
+  test.afterEach(async({page})=>{
+    await page.getByTestId(TEARDOWN_IDS.dropdownIcon).click();
+    await page.getByTestId(TEARDOWN_IDS.deleteButton).click();
+    await page.getByTestId(TEARDOWN_IDS.confirmChechbox).click();
+    await page.getByTestId(TEARDOWN_IDS.finalDelete).click();
+    await expect(page.getByTestId(TEARDOWN_IDS.mainHeader)).toBeVisible();
+  })
+
   test('should create a form and view insights', async ({ page, dashBoard, getFormPage }) => {
-    await dashBoard.createBasicForm();
-    await dashBoard.viewInsights({ visitCount: 0, startCount: 0, submissionCount: 0 });
-    let formPage = await getFormPage();
-    await formPage.pageLookup();
-    await page.reload();
-    await dashBoard.viewInsights({ visitCount: 1, startCount: 0, submissionCount: 0});
-    formPage = await getFormPage();
-    await formPage.pageLookupAndStart();
-    await page.reload();
-    await dashBoard.viewInsights({ visitCount: 2, startCount: 1, submissionCount: 0});
-    formPage = await getFormPage();
-    await formPage.pageLookupAndSubmit();
-    await page.reload();
-    await dashBoard.viewInsights({ visitCount: 3, startCount: 1, submissionCount: 1});
+    await test.step('Step-1: Create a basic form and check the insights', async()=>{
+      await dashBoard.createBasicForm();
+      await dashBoard.viewInsights({ visitCount: 0, startCount: 0, submissionCount: 0 });
+    });
+    await test.step('Step-2: Visit the form and check the insights', async()=>{
+      const formPage = await getFormPage();
+      await formPage.pageLookup();
+      await page.reload();
+      await dashBoard.viewInsights({ visitCount: 1, startCount: 0, submissionCount: 0});
+    })
+    await test.step('Step-3: Start filling the form and check the insights', async()=>{
+      const formPage = await getFormPage();
+      await formPage.pageLookupAndStart();
+      await page.reload();
+      await dashBoard.viewInsights({ visitCount: 2, startCount: 1, submissionCount: 0});
+    })
+    await test.step('Step-4: Submit the form and check the insights', async()=>{
+      const formPage = await getFormPage();
+      await formPage.pageLookupAndSubmit();
+      await page.reload();
+      await dashBoard.viewInsights({ visitCount: 3, startCount: 1, submissionCount: 1});
+      await page
+            .getByRole('link',{ name:'Build' })
+            .click();
+    });
   });
 });
